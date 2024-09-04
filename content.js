1313e7522c7a0cc6d56b1a9e08381acd3d970f20
Isaac <ia4252@trevor.org>
2024-09-03 21:21:18 -0400

    first commit

---
+++ content.js
@@ -0,0 +1,10 @@
+var IndexID = window.location.href.slice(-8);
+function Get(yourUrl){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;
+}
+var UserID = JSON.parse(Get("https://trevor.myschoolapp.com/api/GoogleApi/DriveSettings"))["UserId"]
+var ID = JSON.parse(Get("https://trevor.myschoolapp.com/api/assignment2/UserAssignmentDetailsGetAllStudentData?assignmentIndexId="+IndexID+"&studentUserId="+UserID+"&personaId=2"))["AssignmentId"]
+location.replace("https://trevor.myschoolapp.com/app/student#assignmentdetail/"+ID+"/"+IndexID+"/0/studentmyday--assignment-center")
